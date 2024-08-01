eb6bfb3c7e04fdc4d7549192d066d38e3d92d0b8
EhsanAshoori <ehsanehsan2761@gmail.com>
2024-08-01 23:00:35 +0330

    cleaning default files, adding sass to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import "@/assets/scss/main.scss"
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
